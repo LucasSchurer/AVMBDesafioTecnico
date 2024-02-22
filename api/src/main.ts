eb7cddbdd80c0dcb8dde74d887888722ab60ee48
Lucas Schurer <lschurerm@gmail.com>
2024-02-22 17:20:35 -0300

    Start adding swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ApiModule } from './api.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
+
+  const options = new DocumentBuilder()
+      .setTitle('API')
+      .setDescription('API criada para o desafio técnico da AVMB')
+      .setVersion('1.0')
+      .addServer('http://localhost:3000/', 'Local')
+      .build()
+
+  const document = SwaggerModule.createDocument(app, options)
+  
+  SwaggerModule.setup('docs', app, document)
+
   await app.listen(3000);
 }
 
